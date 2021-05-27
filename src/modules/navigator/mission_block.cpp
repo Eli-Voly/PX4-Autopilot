Eli-Voly/PX4-Autopilot

commit 85d8e74609b9d7c603454238b27c69cb8d80da92

Navigator: yaw error from param and pos_ctl_status.acceptence_rad checks before using
Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

src/modules/navigator/mission_block.cpp
@@ -391,7 +391,7 @@ MissionBlock::is_mission_item_reached()
 				}
 
 
-				if (fabsf(yaw_err) < 0.1f) { //accept heading for exit if below 0.1 rad error (5.7deg)
+				if (fabsf(yaw_err) < _navigator->get_yaw_threshold()) {
 					exit_heading_reached = true;
 				}
 

src/modules/navigator/navigator_main.cpp
@@ -992,15 +992,17 @@ Navigator::get_cruising_throttle()
 float
 Navigator::get_acceptance_radius()
 {
-	if (_vstatus.vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING) {
-		// return the value specified in the parameter NAV_ACC_RAD
-		return get_default_acceptance_radius();
+	float acceptance_radius = get_default_acceptance_radius(); // the value specified in the parameter NAV_ACC_RAD
+	const position_controller_status_s &pos_ctrl_status = _position_controller_status_sub.get();
 
-	} else {
-		// return the max of NAV_ACC_RAD and the controller acceptance radius (e.g. L1 distance)
-		const position_controller_status_s &pos_ctrl_status = _position_controller_status_sub.get();
-		return math::max(pos_ctrl_status.acceptance_radius, get_default_acceptance_radius());
+	// for fixed-wing and rover, return the max of NAV_ACC_RAD and the controller acceptance radius (e.g. L1 distance)
+	if (_vstatus.vehicle_type != vehicle_status_s::VEHICLE_TYPE_ROTARY_WING
+	    && PX4_ISFINITE(pos_ctrl_status.acceptance_radius) && pos_ctrl_status.timestamp != 0) {
+
+		acceptance_radius = math::max(acceptance_radius, pos_ctrl_status.acceptance_radius);
 	}
+
+	return acceptance_radius;
 }
 
 float

commit 772784fb5e639d1dd81ccd29f1b4136ebb7acf9a

navigator: fix acceptance radius for multicopter
This fixes a regression introduced in
https://github.com/PX4/PX4-Autopilot/pull/16646
which meant that the acceptance radius was no longer used at all for
multicopter, and instead only the NAV_ACC_RAD param was used.

With this change we use the acceptance radius of the mission item again
if it is actually set (and not NAN) which we did not do before, and we
only do that for multicopter.

src/modules/navigator/mission_block.cpp
@@ -277,9 +277,15 @@ MissionBlock::is_mission_item_reached()
 			_time_wp_reached = now;
 
 		} else {
-			/*normal mission items */
 
-			float mission_acceptance_radius = _navigator->get_acceptance_radius();
+			float acceptance_radius = _navigator->get_acceptance_radius();
+
+			// We use the acceptance radius of the mission item if it has been set (not NAN)
+			// but only for multicopter.
+			if (_navigator->get_vstatus()->vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING
+			    && PX4_ISFINITE(_mission_item.acceptance_radius)) {
+				acceptance_radius = _mission_item.acceptance_radius;
+			}
 
 			/* for vtol back transition calculate acceptance radius based on time and ground speed */
 			if (_mission_item.vtol_back_transition
@@ -292,13 +298,13 @@ MissionBlock::is_mission_item_reached()
 				const float reverse_delay = _navigator->get_vtol_reverse_delay();
 
 				if (back_trans_dec > FLT_EPSILON && velocity > FLT_EPSILON) {
-					mission_acceptance_radius = ((velocity / back_trans_dec / 2) * velocity) + reverse_delay * velocity;
+					acceptance_radius = ((velocity / back_trans_dec / 2) * velocity) + reverse_delay * velocity;
 
 				}
 
 			}
 
-			if (dist_xy >= 0.0f && dist_xy <= mission_acceptance_radius
+			if (dist_xy >= 0.0f && dist_xy <= acceptance_radius
 			    && dist_z <= _navigator->get_altitude_acceptance_radius()) {
 				_waypoint_position_reached = true;
 			}